866273a0b58f3a9785dbafc3be6dbf5a9a7e258c
guyrefaeli1212-cell <guyrefaeli1212@gmail.com>
2026-07-25 00:06:06 +0300

    Delete core.js

--- core.js
+++
@@ -1,9 +0,0 @@
-discord-mega-bot/
-├── .env
-├── package.json
-├── README.md
-└── src/
-    ├── core.js          # הלב הפועם של הבוט (שקראנו לו בשם החדש!)
-    └── features/        # כאן יישבו כל 100 הפיצרים שלך
-        ├── chaos.js
-        └── games.js
